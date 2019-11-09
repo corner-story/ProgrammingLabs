62ae527d5437c69cc605b795427493a75375e5db
unknown <lambdafate@163.com>
2019-11-09 18:24:56 +0800

    compiler fix bugs

--- CompilerDisplay/src/main/java/com/example/demo/controller/Index.java
+++ CompilerDisplay/src/main/java/com/example/demo/controller/Index.java
@@ -2,27 +2,44 @@ package com.example.demo.controller;
 
 
 import com.compiler.interpreter.Interpreter;
+import com.compiler.lex.Token;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.compiler.interpreter.ByteCode;
-
+import java.util.HashMap;
 import java.util.List;
+
 
 @Controller
 public class Index {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String index(){
-        String source = "int test := 23E+2; printf(test);";
 
-        List<ByteCode> byteCodes = Interpreter.ByteCodes(source);
-        String res = "";
-        for (ByteCode code : byteCodes) {
-            res = res + code.toString() + "\n";
+    @RequestMapping("/")
+    public String index(){
+        return "redirect:/compiler";
+    }
+
+    @RequestMapping("/compiler")
+    public String compiler(){
+        return "index";
+    }
+
+    @PostMapping("/api/tokens")
+    @ResponseBody
+    public Object tokens(@RequestParam String source){
+        HashMap<String, Object> res = new HashMap<>();
+        try{
+            List<Token> tokens = Interpreter.Tokenize(source);
+
+            res.put("code", "200");
+            res.put("data", tokens);
+        }catch (Exception e){
+            System.out.println(e);
         }
+
         return res;
     }
 }
