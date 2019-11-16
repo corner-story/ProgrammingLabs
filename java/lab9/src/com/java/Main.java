19dd425abc0cd637e098052f1c33806fb2ffef30
unknown <lambdafate@163.com>
2019-11-16 13:45:28 +0800

    test

--- java/lab9/src/com/java/Main.java
+++ java/lab9/src/com/java/Main.java
@@ -1,23 +1,44 @@
 package com.java;
 
+
+
+//过山洞
 public class Main {
-    public static void main(String[] args)  throws InterruptedException {
-        HelloThread t = new HelloThread();
-        t.start();
-        Thread.sleep(100);
-        t.running = false; // 标志位置为false
+    public static void main(String[] args) throws Exception{
+        Person[] people = new Person[10];
+        for(int i=0; i<10; i++){
+            people[i] = new Person(String.valueOf(i));
+        }
+        for(int i=0; i<10; i++){
+            people[i].start();
+            people[i].join();
+        }
+
+        System.out.println("\n全部通过山洞!");
     }
 }
 
-class HelloThread extends Thread {
-    public volatile boolean running = true;
-    public void run() {
-        int n = 0;
-        while (running) {
-            n ++;
-            System.out.println(n + " hello!" + running);
-        }
-        System.out.println("end!" + running);
+
+class Person extends Thread{
+    public String name;
+    public Person(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run(){
+        PassHole.pass(this.name);
+    }
+}
+
+
+class PassHole{
+
+    public static synchronized void pass(String name){
+
+        for(int i=0; i<100000; i++){ }
+        System.out.println(name + "\t穿过山洞...");
+
     }
 }
 
