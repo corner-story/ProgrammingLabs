6832719b1899aaf16b8e6e6d49c504e8d2f563c8
lambdafate <lambdafate@163.com>
2020-06-03 17:34:18 +0800

    update lab8, fix display bug.

--- embedded-os/lab8-chatroom/client.c
+++ embedded-os/lab8-chatroom/client.c
@@ -15,8 +15,22 @@
 #define SERVER_ADDR "39.107.83.159"
 #define SERVER_PORT 6666
 
+#define SCREEN_BEGIN_LINE   1
+
+#define SERVER_INS "SERVER-INS"
+#define SERVER_INS_MESSAGE 0
+#define SERVER_INS_USER_COME 1
+#define SERVER_INS_USER_EXIT 2
+
+#define USER_COME_TIPS                  " come in chatroom!"
+#define USER_EXIT_TIPS                  " exit chatroom!"
+#define ONLINE_GAY_TIPS                 "ChatRoom-online-gay: "
+#define ONLINE_GAY_TIPS_LENGTH          (strlen(ONLINE_GAY_TIPS))
+#define USER_COME_TIPS_LENGTH           (strlen(USER_COME_TIPS))
+#define USER_EXIT_TIPS_LENGTH           (strlen(USER_EXIT_TIPS))
+
 int sockfd;
-int next_writed_row = 0;
+int next_writed_row = SCREEN_BEGIN_LINE;
 WINDOW *win;
 char username[12];
 int name_length;
@@ -25,9 +39,8 @@ void *read_user_input(void *sockfd);
 void *recv_from_server(void *sockfd);
 void sigint_handler(int num);
 void refresh_input_box();
-void write_display_win(char buffer[]);
 void init_client(int sockfd);
-void write_and_refresh(char buffer[]);
+void write_display_win(char buffer[]);
 
 int main(int argc, char *argv[]){
     printf("please input your name(length between 1~12): ");
@@ -43,6 +56,7 @@ int main(int argc, char *argv[]){
 
     setlocale(LC_ALL, "");
     win = initscr(); /* 初始化屏幕 */
+
     curs_set(TRUE);
     clear();
     refresh_input_box();
@@ -85,7 +99,7 @@ void *read_user_input(void *sockfd){
     while(1){
         getstr(buffer);
         send(fd, buffer, strlen(buffer), 0);
-        // write_and_refresh(buffer);
+        refresh_input_box();
     }
 }
 
@@ -95,13 +109,9 @@ void *recv_from_server(void *sockfd){
     int n = -1;
     while((n = recv(fd, buffer, 1024, 0)) > 0){
         buffer[n] = '\0';
-        write_and_refresh(buffer);
+        write_display_win(buffer);
+        
     }
-}
-
-void write_and_refresh(char buffer[]){
-    write_display_win(buffer);
-    refresh_input_box();
 }
 
 void sigint_handler(int num){
@@ -122,29 +132,78 @@ void refresh_input_box(){
     for (int i = 0; i < clo; i++){
         printw("*");
     }
+    move(line_row+1, 0);
     refresh();
 }
 
 void write_display_win(char buffer[]){
+    // save current position
+    int old_row, old_clo;
+    getyx(win, old_row, old_clo);
+
+    int cur_row, cur_clo;
+
     int row, clo;
     getmaxyx(stdscr, row, clo);
     int line_row = row * 4 / 5;
     int length = strlen(buffer);
     int last_row = next_writed_row + length / clo + (length % clo == 0 ? 0 : 1);
     if (last_row >= line_row){
-        clear();
-        next_writed_row = 0;
+        for (int i = SCREEN_BEGIN_LINE; i < line_row; i++){
+            move(i, 0);
+            clrtoeol();
+        }
+        next_writed_row = SCREEN_BEGIN_LINE;
     }
+
+    char *begin = buffer;
+    char *ins_header = strsep(&begin, "&");
+    char *ins_num = strsep(&begin, "&");
+    char *online_gay = strsep(&begin, "&");
+    char *time = strsep(&begin, "&");
+    char *message = begin;
+
+    // update online gay
+    move(0, 0);
+    int online_gay_begin_clo = (clo - strlen(online_gay) - ONLINE_GAY_TIPS_LENGTH) / 2;
+    for (int i = 0; i < online_gay_begin_clo; i++){
+        printw("*");
+    }
+    move(0, online_gay_begin_clo);
+    printw("%s%s", ONLINE_GAY_TIPS, online_gay);
+    getyx(win, cur_row, cur_clo);
+    for (int i = cur_clo; i < clo; i++){
+        printw("*");
+    }
+    refresh();
+    
     // write data
     move(next_writed_row, 0);
-    char *begin = buffer;
-    char *username = strsep(&begin, "&");
-    char *time = strsep(&begin, "&");
-    char *data = begin;
-    printw("[%s] [%s] say:\n%s", username, time, data);
+    switch (atoi(ins_num))
+    {
+        case SERVER_INS_MESSAGE:
+            printw("[%s] [%s] say:\n%s", ins_header, time, message);
+            break;
+        case SERVER_INS_USER_COME:
+            move(next_writed_row, (clo-strlen(message)-USER_COME_TIPS_LENGTH)/2);
+            printw("%s%s", message, USER_COME_TIPS);
+            break;
+        case SERVER_INS_USER_EXIT:
+            move(next_writed_row, (clo-strlen(message)-USER_EXIT_TIPS_LENGTH)/2);
+            printw("%s%s", message, USER_EXIT_TIPS);
+            break;
+        default:
+            printw("[%s] [%s] say:\n%s", "no-name gay", "just now", "fuck! it means occured bugs if you see this line!");
+            break;
+    }
+
     // mvprintw(next_writed_row, 0, buffer);
     refresh();
     // update next_writed_row
     getyx(win, row, clo);
     next_writed_row = (clo == 0 ? row : (row + 1));
+
+    // back to old position
+    move(old_row, old_clo);
+    refresh();
 }
--- embedded-os/lab8-chatroom/server.c
+++ embedded-os/lab8-chatroom/server.c
@@ -9,10 +9,22 @@
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
+#include <stdarg.h>
 #include <assert.h>
+
+/*
+    server->clients message formate:
+    [SERVER_INS or sender's name] & [SERVER_INS_xxx] &  [online gay] & [update time] & [message]
+
+*/
 
 #define SERVER_PORT 6666
 #define MAX_CLIENT_NUM  32
+#define SERVER_INS      "SERVER-INS"
+#define SERVER_INS_FMT  "%s&%d&%d&%s&%s"
+#define SERVER_INS_MESSAGE      0
+#define SERVER_INS_USER_COME    1
+#define SERVER_INS_USER_EXIT    2
 
 struct client_t {
     pthread_t thread;
@@ -32,9 +44,14 @@ char *get_time();
 struct client_t *get_client();
 void send_all(char buffer[], int length);
 void init_client();
-void clear_client(pthread_t thread);
+void clear_client(struct client_t *client);
 void delete_clients();
 struct client_t *get_client_from_sockfd(int sockfd);
+void send_server_ins(char *fmt, ...);
+int online_gay();
+void forward_server_ins(int ins_num, char *message);
+void forward_message(char *username, char *message);
+
 
 
 int main(int argc, char *argv[])
@@ -94,6 +111,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+
 void *request_handler(void* sockfd){
     int fd = *((int*)sockfd);
     printf("current thread: %ld socket: %d\n", pthread_self(), fd);
@@ -101,38 +119,30 @@ void *request_handler(void* sockfd){
     char buffer[1024];
     char data[1024];
     int n = -1;
-    // read username
+    // read username, user come in chat-room
     if ((n = recv(fd, buffer, 1024, 0)) > 0){
-        strncpy(client->username, buffer, n);
+        buffer[n] = '\0';
+        strcpy(client->username, buffer);
         client->name_length = n;
         strncpy(data, buffer, n);
+        data[n] = '&';
     }
-    data[n] = '&';
+
+    // send 'user come in chatroom msg'
+    forward_server_ins(SERVER_INS_USER_COME, client->username);
+
     while ((n = recv(fd, buffer, 1024, 0)) > 0){
         buffer[n] = '\0';
         printf("[%s]: %s\n", client->username, buffer);
-
-        // add time
-        char *time = get_time();
-        int time_length = strlen(time);
-        strncpy(&data[client->name_length+1], time, time_length);
-
-        // add buffer data
-        int index = client->name_length+1+time_length;
-        data[index] = '&';
-        strcpy(&data[index+1], buffer);
-
-        // add '\n'(send data right now) and '\0'(for printf)
-        int data_length = index+1+n;
-        data[data_length] = '\n';
-        data[data_length+1] = '\0';
-        printf("[%s]: %s\n", "SEND_ALL", data);
-        
-        // send data to all clients
-        send_all(data, data_length+1);
+        forward_message(client->username, buffer);
     }
+
+
+    // user get out of the chat room
     close(fd);
-    clear_client(pthread_self());
+    clear_client(client);
+    // send 'user get out of the chatroom' msg
+    forward_server_ins(SERVER_INS_USER_EXIT, client->username);
     pthread_exit(NULL);
 }
 
@@ -194,14 +204,10 @@ struct client_t *get_client_from_sockfd(int sockfd){
     return NULL;
 }
 
-void clear_client(pthread_t thread){
+void clear_client(struct client_t *client){
     pthread_mutex_lock(&mutex);
-    for (int i = 0; i < MAX_CLIENT_NUM; i++){
-        if(clients[i].thread == thread){
-            clients[i].sockfd = 0;
-            clients[i].thread = 0;
-        }
-    }
+    client->sockfd = 0;
+    client->thread = 0;
     pthread_mutex_unlock(&mutex);
 }
 
@@ -211,4 +217,38 @@ void delete_clients(){
             close(clients[i].sockfd);
         }
     }
+}
+
+int online_gay(){
+    int gay = 0;
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < MAX_CLIENT_NUM; i++){
+        if (clients[i].sockfd != 0 && clients[i].thread != 0){
+            gay++;
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+    return gay;
+}
+
+// forward one client's message to all clients
+void forward_message(char *username, char *message){
+    send_server_ins(SERVER_INS_FMT, username, SERVER_INS_MESSAGE, online_gay(), get_time(), message);
+}
+
+void forward_server_ins(int ins_num, char *message){
+    send_server_ins(SERVER_INS_FMT, SERVER_INS, ins_num, online_gay(), get_time(), message);
+}
+
+void send_server_ins(char *fmt, ...){
+    char buffer[1024];
+    va_list args;
+    va_start(args, fmt);
+    vsprintf(buffer, fmt, args);
+    va_end(args);
+
+    int length = strlen(buffer);
+    buffer[length+1] = '\n';
+    printf("[%s]: %s\n", "SEND_ALL", buffer);
+    send_all(buffer, length);
 }
